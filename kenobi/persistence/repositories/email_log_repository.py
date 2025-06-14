463e85dcb4efb57b107ca32f537a5742c1a4886d
Eduardo Lemos <eduardo.lemosinc@gmail.com>
2025-06-14 17:55:43 -0300

    Feat(EmailLog): Adding logger to Kenobi EmailLog

--- kenobi/persistence/repositories/email_log_repository.py
+++ kenobi/persistence/repositories/email_log_repository.py
@@ -1,10 +1,20 @@
 #kenobi/db/repositories/email_log_repository.py
+
+import logging
 from kenobi.persistence import SessionLocal
 from kenobi.persistence.entities.email_log_entity import EmailLog
 from kenobi.dtos.email_log_dto import EmailLogDTO
 
+# logging setup
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(funcName)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
 def save_email_log(data: dict) -> EmailLog:
     session = SessionLocal()
+    logger.info("DB Session Open.")
     try:
         log = EmailLog(**data)
         session.add(log)
@@ -12,24 +22,41 @@ def save_email_log(data: dict) -> EmailLog:
 
         entity = EmailLog.copy(log)  # Ainda está vinculado à sessão
         session.commit()
+        logger.info("Data commited to DB.")
         return entity
     except Exception as e:
+        logger.exception(f"Data cloud not be saved. Raised exception: {e}")
         session.rollback()
         raise e
     finally:
         session.close()
+        logger.info("DB Session Closed.")
 
 
 def get_email_log_by_id(log_id: str):
     session = SessionLocal()
+    logger.info("DB Session Open.")
     try:
-        return session.query(EmailLog).filter(EmailLog.id == log_id).first()
+        entity = session.query(EmailLog).filter(EmailLog.id == log_id).first()
+        logger.info(f"Entity recovered from DB.")
+        return entity
+    except Exception as e:
+        logger.exception(f"Data cloud not be recovered. Raised exception: {e}")
+        raise e
     finally:
         session.close()
+        logger.info("DB Session Closed.")
 
 def get_all_email_logs_paginated(skip: int = 0, limit: int = 10):
     session = SessionLocal()
+    logger.info("DB Session Open.")
     try:
-        return session.query(EmailLog).offset(skip).limit(limit).all()
+        entities = session.query(EmailLog).offset(skip).limit(limit).all()
+        logger.info(f"Number of entities recovered: {len(entities)}")
+        return entities
+    except Exception as e:
+        logger.exception(f"Data cloud not be recovered. Raised exception: {e}")
+        raise e
     finally:
         session.close()
+        logger.info("DB Session Closed.")
--- kenobi/services/email_log_service.py
+++ kenobi/services/email_log_service.py
@@ -1,10 +1,21 @@
 #kenobi/services/email_logs_service.py
 
+import logging
 from datetime import datetime, timedelta, timezone
 from kenobi.dtos.email_log_dto import EmailLogDTO
 from kenobi.persistence.repositories import email_log_repository
 
+# logging setup
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(funcName)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
 def create_email_log(dto: EmailLogDTO):
+
+    logger.info("Started create email log on DB.")
+
     data = {
         "subject": dto.subject,
         "recipients": dto.recipients,
@@ -16,14 +27,26 @@ def create_email_log(dto: EmailLogDTO):
         "sent_at": dto.sent_at,
         "api_response_code" : dto.api_response_code
     }
+
     saved = email_log_repository.save_email_log(data)
+
+    logger.info(f"Email log created with ID: {saved.id}")
+
     return EmailLogDTO.from_entity(saved)
 
 #untested
 def get_email_log_by_id(log_id: int) -> EmailLogDTO:
+
+    logger.info("Started GET email log on DB.")
+
     log = email_log_repository.get_email_log_by_id(log_id)
+
     return EmailLogDTO.from_entity(log) if log else None
 
 def get_all_email_logs_paginated(skip: int = 0, limit: int = 10):
+
+    logger.info("Started GET ALL email log on DB.")
+
     logs = email_log_repository.get_all_email_logs_paginated(skip, limit)
-    return [EmailLogDTO(**log.__dict__) for log in logs]
+
+    return [EmailLogDTO.from_entity(log) for log in logs]
